ae39ffc918f790bfd8784ee854f8e589d369aea2
Paolo Francesco Pompa <117436985+P4020@users.noreply.github.com>
2024-05-30 12:46:37 +0200

    Add files via upload

---
+++ Migrations/20240530104408_quinta versione.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class quintaversione : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Immagine",
+                table: "Carrello",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Immagine",
+                table: "Carrello");
+        }
+    }
+}
